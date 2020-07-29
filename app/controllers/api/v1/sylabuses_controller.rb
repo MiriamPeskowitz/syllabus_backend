41111c4b3668220a1de11bef3c4e13a3d7b1b448
MiriamPeskowitz <miriam5000@gmail.com>
2020-07-29 14:23:18 -0400

    Sylabus controller index and create routes

--- app/controllers/api/v1/sylabuses_controller.rb
+++ app/controllers/api/v1/sylabuses_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::SylabusesController < ApplicationController
+
+	def index
+		sylabuses = Sylabus.all
+		render json: sylabuses
+	end
+
+	def create
+		sylabus = Sylabus.new(sylabus_params)
+		if sylabus.save 
+			render json: sylabus, status: :accepted 
+		else
+			render json: {errors: sylabus.errors.full_messages}, status: :unprocessable_entity
+		end 
+	end
+
+
+private
+	def sylabus_params
+		params.require(:sylabus).permit(:title, :description, :image_url, :category_id)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	namespace :api do
 		namespace :v1 do
-			resources :sylabuses, only: [:index]
+			resources :sylabuses, only: [:index, :create]
 		end 
 	end 
 end
